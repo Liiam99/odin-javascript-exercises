b6d8c90db9b1064bc1ce07910972e5d2fbe112a8
Liam <liam99mail@proton.me>
2025-02-26 11:34:56 +0100

    Add my solution for the fifth exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,31 @@
-const sumAll = function() {
+const sumAll = function(first, second) {
+    if (!isPosInt(first) || !isPosInt(second)) {
+        return 'ERROR';
+    }
 
+    let highest, lowest;
+    if (first > second) {
+        highest = first;
+        lowest = second;
+    } else {
+        highest = second;
+        lowest = first;
+    }
+
+    // Gaus summation that supports start value higher than 1.
+    const sum = (highest*(highest + 1))/2 - (lowest*(lowest - 1))/2
+
+    return sum;
 };
+
+
+function isPosInt(value) {
+    if (typeof value == 'number' && value > 0 && Number.isInteger(value)) {
+        return true;
+    }
+
+    return false;
+}
 
 // Do not edit below this line
 module.exports = sumAll;
