a8793b2e5dfdd2985bc60018e22112b210b4eaff
Liam <liam99mail@proton.me>
2025-03-05 11:27:16 +0000

    Add my solutions for the 9th JS exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,19 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const arr = string.toLowerCase().split('');
+    const filteredArr = arr.filter((character) => isValidCharacter(character));
 
+    const cleanedString = filteredArr.join('');
+    const reverseString = filteredArr.reverse().join('');
+
+    return reverseString === cleanedString;
 };
+
+function isValidCharacter(character) {
+    const isDigit = character >= '0' && character <= '9';
+    const isLetter = character >= 'a' && character <= 'z';
+
+    return isDigit || isLetter ? true : false;
+}
 
 // Do not edit below this line
 module.exports = palindromes;
