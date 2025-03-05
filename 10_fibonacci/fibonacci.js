f6a8c2bbc507a5d026451cfdd23ac04356d32253
Liam <liam99mail@proton.me>
2025-03-05 11:53:49 +0000

    Add my solutions for the 10th JS exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(fiboNumber) {
+    if (fiboNumber == 0) {
+        return 0;
+    } else if (fiboNumber < 0) {
+        return 'OOPS';
+    }
 
+    let swapValue = null;
+    let prevValue = 0;
+    let value = 1;
+
+    for (let i = 1; i < fiboNumber; i++) {
+        swapValue = value;
+        value += prevValue;
+        prevValue = swapValue;
+    }
+
+    return value;
 };
 
 // Do not edit below this line
